9c2588baa1cf85818c7eb031eee7eb21a34eaf43
James McConnell <james.mcc90@gmail.com>
2024-08-19 10:46:10 +0100

    Add files via upload

---
+++ python_test/python_test/MyPython.py
@@ -0,0 +1,13 @@
+import pandas as pd
+print("Hello, world") 
+
+df = pd.read_excel('C:/Users/james/OneDrive/Desktop/JobTracker.xlsx', sheet_name='Sheet2')
+
+print(df)
+
+def myFunction():
+    names = ["James", "Emma", "Noah", "Faith", "Gatsby"]
+    for name in names:
+        print(name + " McConnell")
+
+myFunction()
